37465e3ee54d6be25022f0737154da32395c7acf
SailorCodes <42174576+SailorCodes@users.noreply.github.com>
2024-07-28 14:12:49 +0300

    SailorCodes

---
+++ src/constants/index.js
@@ -0,0 +1,45 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate Software Developer with a knack for crafting robust and scalable  applications. With 3 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, C#, Php. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile Software Developer with a passion for creating efficient and user-friendly  applications. With 3 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, C#, and Php. My journey in Software Developer began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const PROJECTS = [
+  {
+    title: "Gemini-Clone",
+    image: project1,
+    description:
+      "Google gemini app using React.js and generative ai.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "Gemini API"],
+  },
+  {
+    title: "Heart Attack Prediction System",
+    image: project2,
+    description:
+      "This tool is primarily designed for the medical sector to analyze and store data in text form. Managing data can be difficult, and it often takes up a lot of time to find data on a particular person. With this tool, you can easily find any data you need in just a few basic steps.",
+    technologies: ["Python", "MySQL database"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS" , "Next.Js"],
+  },
+  {
+    title: "Mobile Games",
+    image: project4,
+    description:
+      "A project that includes multiple mobile games. 7 games, each of which I coded using Unity.",
+    technologies: ["C#", "Unity"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kağıthane İstanbul ",
+  phoneNo: "+90 546 216 60 19 ",
+  email: "ardag3781@gmail.com",
+};
